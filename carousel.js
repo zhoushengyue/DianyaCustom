595f7c29902672629486cb813291d90be69ee4bc
zhoushengyue <zhoushengyue@gmail.com>
2019-08-22 17:22:29 +0800

    更新导航

--- carousel.js
+++ carousel.js
@@ -11,7 +11,7 @@ map.add(marker);//添加到地图
 
 var infoWindow = new AMap.InfoWindow({ //创建信息窗体
   isCustom: true,  //使用自定义窗体
-  content:'<div><p><b><a href="amapuri://route/plan/?sname=我的位置&dlat=28.184624&dlon=113.016101&dname=长沙典雅橱柜&dev=0&t=0">点此导航到店</a></b></p></div>'
+  content:'<div><p><b><a href="http://api.map.baidu.com/marker?location=28.184624,113.016101&title=长沙典雅橱柜&coord_type=gcj02&content=高桥建材市场北门1号&output=html&src=webapp.baidu.openAPIdemo">点此导航到店</a></b></p></div>'
   //offset:new Amap.Pixel(5,-5)
 });
 var onMarkerClick  =  function(e) {
